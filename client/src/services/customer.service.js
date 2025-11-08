269de3f8928f13ce86bf5266396932c9035f2897
Darsh Prajapati <darshprajapati1510@gmail.com>
2025-11-08 23:00:08 +0530

    Update customer.service.js

--- client/src/services/customer.service.js
+++ client/src/services/customer.service.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL+'/api/customers';
+const API_URL = import.meta.env.VITE_API_URL+"/api/customers";
 
 const api = axios.create({
   baseURL: API_URL,
