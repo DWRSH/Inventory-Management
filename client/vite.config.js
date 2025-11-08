f0f264dd25f03e9e4294e070165e22665af25ccb
Darsh Prajapati <darshprajapati1510@gmail.com>
2025-11-08 21:35:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // 🔥 necessary for React Router
   build: {
     outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // ensures fallback for client routes during dev
   },
 })
